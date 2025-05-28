58aae9339e1d989e5ae41061bbb698436e10e794
Troels Brødsgaard <tbg@energinet.dk>
2025-05-28 14:28:12 +0200

    wip Add wrapper for MmsServer_installFileAccessHandler

--- dotnet/IEC61850forCSharp/MmsServer.cs
+++ dotnet/IEC61850forCSharp/MmsServer.cs
@@ -34,15 +34,55 @@ namespace IEC61850
 	{
         public class MmsServer : IDisposable
 		{
+
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            // TODO: Should this really be int? I would expect it to run an MmsError type thing?
+            // Which means I need to add an MmsError type, since it seems to not exist in dotnet land
+            // Maybe I can take inspiration from MmsDataAccessError (CommonAPI)
+
+			private delegate int InternalFileAccessHandler(IntPtr parameter, IntPtr mmsServerConnection, IntPtr mmsService, string localFileName, string otherFilename);
+
+            // TODO: I really have no idea what the arguments or return type here should be
+            public delegate int FileAccessHandler(string localFilename, string otherFilename);
+
+            // Look for IedServer_setConnectionIndicationHandler
+            [DllImport("iec61850", CallingConvention = CallingConvention.Cdecl)]
+            static extern void MmsServer_installFileAccessHandler(IntPtr self, InternalFileAccessHandler handler, IntPtr parameter);
+
+            // TODO: The mmsService is actually an enum of MmsFileServiceType (mms_server.h) - it should be defined and passed to the callback (userProvidedFileAccessHandler)
+
+            private int FileAccessHandlerImpl(IntPtr parameter, IntPtr mmsServerConnection, IntPtr mmsService, string localFilename, string otherFilename)
+            {
+                if (userProvidedFileAccessHandler != null)
+                    return userProvidedFileAccessHandler(localFilename, otherFilename);
+
+                // TODO: Does this really make sense?
+                return 0;
+            }
+
+            public void InstallFileAccessHandler(FileAccessHandler handler)
+            {
+                if (internalFileAccessHandler == null)
+                {
+                    internalFileAccessHandler = new InternalFileAccessHandler(FileAccessHandlerImpl);
+                    MmsServer_installFileAccessHandler(self, internalFileAccessHandler, IntPtr.Zero);
+                }
+
+                userProvidedFileAccessHandler = handler;
+            }
+
             [DllImport("iec61850", CallingConvention = CallingConvention.Cdecl)]
             static extern void MmsServer_destroy(IntPtr self);
 
             private IntPtr self = IntPtr.Zero;
+            private InternalFileAccessHandler internalFileAccessHandler = null;
+            private FileAccessHandler userProvidedFileAccessHandler = null;
 
-			internal MmsServer (IntPtr server)
-			{
-				self = server;
-			}
+
+            internal MmsServer(IntPtr server)
+            {
+                self = server;
+            }
 
             public void Dispose()
             {
@@ -52,6 +92,8 @@ namespace IEC61850
                     {
                         MmsServer_destroy(self);
                         self = IntPtr.Zero;
+                        internalFileAccessHandler = null;
+                        userProvidedFileAccessHandler = null;
                     }
                 }
             }
